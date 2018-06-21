5206a181de140899fb949095a19d417e37aa12ed
vsnyc <vsnyc@users.noreply.github.com>
2018-06-21 13:18:09 -0400

    Updated poller to compute score and save to dynamodb

--- ddr_server/sqs_poller.py
+++ ddr_server/sqs_poller.py
@@ -5,29 +5,24 @@ import traceback
 import time
 import sys
 import zmq
+import decimal
 from heapq import *
+import re
 from multiprocessing import Pool
 from ddr_server import ddr_config
 from ddr_server import ddb_util
+from ddr_server import ddr_score
 
 s3bucket = ddr_config.get_config('s3_bucket')
 s3region = ddr_config.get_config('s3_bucket_region')
 queueUrl = ddr_config.get_config('sqs_url')
 numServers = int(ddr_config.get_config('num_op_servers'))
-processed_table = ddr_config.get_config('ddr_processed_table')
+jediMastersTable = ddr_config.get_config('jedi_masters_table')
 
 
 
 def poll_sqs():
-    s3bucket = ddr_config.get_config('s3_bucket')
-    s3region = ddr_config.get_config('s3_bucket_region')
-    queueUrl = ddr_config.get_config('sqs_url')
-    numServers = int(ddr_config.get_config('num_op_servers'))
-    processed_table = ddr_config.get_config('ddr_processed_table')
-
     sqs = boto3.client('sqs', region_name=s3region)
-    s3 = boto3.resource('s3', region_name=s3region)
-    rekognition = boto3.client('rekognition', region_name=s3region)
 
     response = sqs.receive_message(
         QueueUrl=queueUrl
@@ -59,10 +54,10 @@ def poll_sqs():
             # result = processOpenpose(s3, s3bucket, s3filename, filename, serverIndex, numServers)
             # rekResponse = processRekognition(rekognition, s3bucket, s3filename)
             opAsync = pool.apply_async(processOpenpose, [s3bucket, s3filename, filename, serverIndex, numServers])
-            rekAsync = pool.apply_async(processRekognition,[s3bucket, s3filename])
+            # rekAsync = pool.apply_async(processRekognition,[s3bucket, s3filename])
 
             result = opAsync.get(timeout=10)
-            rekResponse = rekAsync.get(timeout=10)
+            # rekResponse = rekAsync.get(timeout=10)
 
             elapsed_time = time.time() - start_time
 
@@ -72,8 +67,9 @@ def poll_sqs():
             pool.join()
 
             if result == "Done.":
-                print("Rekognition response\n" + str(rekResponse))
-                ddb_util.log_processed(filename, processed_table, rekResponse)
+                print("Processed image {}".format(s3filename))
+                # print("Rekognition response\n" + str(rekResponse))
+                # ddb_util.log_processed(filename, processed_table, rekResponse)
 
 
 
@@ -86,32 +82,14 @@ def poll_sqs():
         # traceback.print_exc()
         print("No messages in sqs")
 
+def num_groups(regex):
+    return re.compile(regex).groups
 
-def processRekognition(s3bucket, s3filename):
-
-    start_time = time.time()
-    rekognition = boto3.client('rekognition', region_name=s3region)
-    response = rekognition.detect_faces(Image={'S3Object':{'Bucket':s3bucket,'Name':s3filename}},Attributes=['ALL'])
-    emotions = []
-    smiles = []
-    rek_response = {}
-
-    for faceDetail in response['FaceDetails']:
-        emotions.append(faceDetail['Emotions'])
-        smiles.append(faceDetail['Smile'])
-
-        #print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
-        #      + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
-        #print('Here are the other attributes:')
-        #print(json.dumps(faceDetail, indent=4, sort_keys=True))
-
-    rek_response['Emotions'] = emotions
-    rek_response['Smiles'] = smiles
-
-    elapsed_time = time.time() - start_time
-    print("Processed rekognition in: " + str(elapsed_time))
-    return rek_response
-
+def imageInfo(fileName):
+    regex = '(.*)(_pose)([0-9])(.jpg)'
+    p = re.compile(regex)
+    m = p.match(fileName)
+    return m.group(1), m.group(3)
 
 def processOpenpose(s3bucket, s3filename, filename, serverIndex, numServers):
 
@@ -120,6 +98,14 @@ def processOpenpose(s3bucket, s3filename, filename, serverIndex, numServers):
 
     s3.Bucket(s3bucket).download_file(s3filename, os.path.expanduser('~') + '/' + s3filename)
     result = processImage(filename, 5555 + serverIndex % numServers)
+
+    (nickname, poseNum) = imageInfo(filename)
+    jediJsonFile = os.path.expanduser('~') + '/json/jedi_pose{}.json'.format(poseNum)
+    nicknameJsonFile = os.path.expanduser('~') + '/json/{}_pose{}.json'.format(nickname, poseNum)
+    poseScore = ddr_score.fetch_score(jediJsonFile, nicknameJsonFile)
+
+    saveScore(nickname, poseNum, poseScore)
+
     print("Server result: " + str(result))
     if result == "Done.":
         # heappush(fileQueue, filename)
@@ -132,6 +118,20 @@ def processOpenpose(s3bucket, s3filename, filename, serverIndex, numServers):
 
     return result
 
+def saveScore(nickname, poseNum, score):
+    dynamodb = boto3.resource('dynamodb', region_name=s3region)
+    table = dynamodb.Table(jediMastersTable)
+    response = table.update_item(
+        Key={
+            'nick_name': nickname
+        },
+        UpdateExpression="set scores.pose" + str(poseNum) + " = :s",
+        ExpressionAttributeValues={
+            ':s': decimal.Decimal(score)
+        },
+        ReturnValues="UPDATED_NEW"
+    )
+    print(json.dumps(response, indent=4, cls=ddb_util.DecimalEncoder))
 
 
 def processImage(filename, serverIndex):
