2fcf0201e4532bcc9f7cc2de44f86c69b70844fe
vsnyc <vsnyc@users.noreply.github.com>
2018-06-21 11:31:13 -0400

    Added OpenPose instance, SQS to template

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -3,4 +3,4 @@ cd /home/ubuntu/ddr
 nohup python -m ddr_server.ddr_runner > /home/ubuntu/ddr_runner.log 2>&1 &
 nohup python -m ddr_server.sqs_poller > /home/ubuntu/sqs_poller.log 2>&1 &
 cd /home/ubuntu/openpose
-nohup ./build/examples/tutorial_pose/op_server.bin > /home/ubuntu/openpose.log 2>&1 &
+nohup ./build/examples/tutorial_pose/op_server.bin --disable_blending > /home/ubuntu/openpose.log 2>&1 &
