c59deacd2a37694c3e488eb072b6f37c897aa29d
vsnyc <vsnyc@users.noreply.github.com>
2018-06-25 01:35:37 -0400

    Added affine transformation for the input image to improve pose match calculation

--- ddr_server/ddr_score.py
+++ ddr_server/ddr_score.py
@@ -2,7 +2,7 @@ import json
 import math
 import sys
 import os
-
+import numpy as np
 
 def load_file(input_file):
     with open(input_file) as data_file:
@@ -17,7 +17,7 @@ def jedi_match(json1, json2):
     return pers_score
 
 
-def pose_match(pose_points1, pose_points2):
+def pose_match(model_points, input_points):
     """
     # // Result for COCO (18 body parts)
     # // POSE_COCO_BODY_PARTS {
@@ -47,20 +47,27 @@ def pose_match(pose_points1, pose_points2):
     :param pose_points2: (x2, y2, c2 ...) array of input model
     :return: similarity score
     """
-    num_points = int(min(len(pose_points1), len(pose_points2)) / 3)
+    num_points = int(min(len(model_points), len(input_points)) / 3)
+    pose_points1 = feature_vector(model_points)
+    print(pose_points1)
+    pose_points2 = affine_transform(model_points, input_points)
+    print(pose_points2)
     pt_distances = []
     pt_proximity = []
-    imp_points = range(0, 14) # Ignore REye, LEye, REar, LEar, Background
+    imp_points = range(0, 18)
+
+    dist = np.linalg.norm(pose_points1 - pose_points2)
+    print("Distance: {}".format(dist))
     for i in imp_points:
-        x1 = pose_points1[3 * i]
-        y1 = pose_points1[3 * i + 1]
-        c1 = pose_points1[3 * i + 2]
-        x2 = pose_points2[3 * i]
-        y2 = pose_points2[3 * i + 1]
-        c2 = pose_points2[3 * i + 2]
+        x1 = pose_points1[i][0]
+        y1 = pose_points1[i][1]
+
+        x2 = pose_points2[i][0]
+        y2 = pose_points2[i][1]
+
         pt_distance = math.sqrt(math.pow(x1 - x2, 2) + math.pow(y1 - y2, 2))
         pt_distances.append(pt_distance)
-        pt_pct_closeness = max(100.0 - pt_distance, 20.0)
+        pt_pct_closeness = max(100.0 - pt_distance, 40.0)
         pt_proximity.append(pt_pct_closeness)
 
     total_displacement = 0.0
@@ -73,6 +80,39 @@ def pose_match(pose_points1, pose_points2):
 
     return total_pct_proximity / len(imp_points)
 
+def affine_transform(pose_points1, pose_points2):
+    model_features = feature_vector(pose_points1)
+    input_features = feature_vector(pose_points2)
+
+    # In order to solve the augmented matrix (incl translation),
+    # it's required all vectors are augmented with a "1" at the end
+    # -> Pad the features with ones, so that our transformation can do translations too
+    pad = lambda x: np.hstack([x, np.ones((x.shape[0], 1))])
+    unpad = lambda x: x[:, :-1]
+
+    # Pad to [[ x y 1] , [x y 1]]
+    Y = pad(model_features)
+    X = pad(input_features)
+
+    # Solve the least squares problem X * A = Y
+    # and find the affine transformation matrix A.
+    A, res, rank, s = np.linalg.lstsq(X, Y, rcond=None)
+    A[np.abs(A) < 1e-10] = 0  # set really small values to zero
+
+    transform = lambda x: unpad(np.dot(pad(x), A))
+    input_transform = transform(input_features)
+    return input_transform
+
+def feature_vector(pose_points):
+    # input_features = [[x1,y2],[x2,y2],...]
+    features = []
+    for i in range(0,18):
+        xi = pose_points[3 * i]
+        yi = pose_points[3 * i + 1]
+        ci = pose_points[3 * i + 2] # Ignored
+        ithCoordinate = [xi, yi]
+        features.append(ithCoordinate)
+    return np.array(features)
 
 def fetch_score(file1, file2):
     """
@@ -92,7 +132,7 @@ def main(argv):
 
     pose_match_result = fetch_score(file1, file2)
 
-    print(pose_match_result)
+    print("Score: " + str(pose_match_result))
 
     return
 
